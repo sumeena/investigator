2d281adb8349011951dbb01db6cd3a084ceef68c
dev05 <devod0485@gmail.com>
2023-11-15 20:03:39 +0530

    added content to login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,10 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
+            <h2>Servicing the the Greater San Francisco<br>Bay Area, San Jose, Sacramento, Fresno<br>and Southern California since 1969.</h2>
+        </div>
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
 
@@ -19,9 +22,9 @@
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -33,9 +36,9 @@
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
                         </div>
@@ -60,17 +63,123 @@
 
                                 <a href="/register" class="btn btn-primary">
                                     {{ __('Register') }}
-                                </button>
+                                    </button>
 
-                                @if (Route::has('password.request'))
+                                    @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
-                                @endif
+                                    @endif
                             </div>
                         </div>
                     </form>
                 </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="card mt-4">
+        <div class="row mt-4 our-services justify-content-center">
+            <div class="col-md-12">
+                <h2 class="text-center"><b>Our Services</b></h2>
+            </div>
+            <div class="col-md-4 mt-4">
+                <ul>
+                    <li>
+                        <p><img width="20" src="{{ asset('html/green_tick.png') }}"> INVESTIGATION</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4 mt-4">
+                <ul>
+                    <li>
+                        <p><img width="20" src="{{ asset('html/green_tick.png') }}"> ACTIVITY CHECK</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4 mt-4">
+                <ul>
+                    <li>
+                        <p><img width="20" src="{{ asset('html/green_tick.png') }}"> SOCIAL MEDIA CHECKS</p>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-md-4 mt-4">
+                <ul>
+                    <li>
+                        <p><img width="20" src="{{ asset('html/green_tick.png') }}"> BACKGROUND CHECKS</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4 mt-4">
+                <ul>
+                    <li>
+                        <p><img width="20" src="{{ asset('html/green_tick.png') }}"> AOE/COE INVESTIGATIONS</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4 mt-4">
+                <ul>
+                    <li>
+                        <p><img width="20" src="{{ asset('html/green_tick.png') }}"> RECORDED STATEMENT INVESTIGATIONS</p>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="col-md-4 mt-4">
+                <ul>
+                    <li>
+                        <p><img width="20" src="{{ asset('html/green_tick.png') }}"> TRANSCRIPTION</p>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-8 mt-4">
+                <ul>
+                    <li>
+                        <p><img width="20" src="{{ asset('html/green_tick.png') }}"> PHARMACY/MEDICAL SWEEP INVESTIGATIONS</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <div class="card mt-4">
+        <div class="row mt-4 our-services justify-content-center">
+            <div class="col-md-12">
+                <h2 class="text-center"><b>About US</b></h2>
+            </div>
+
+            <div class="mt-4 col-md-12">
+                <p class="text-center">iLogisticsinc is an Investigation Professionals Company of experience, communication and results.</p>
+                <p class="text-center">Our platform provides services for companies looking for surveillance services and for surveillance investigator projects.</p>
+            </div>
+
+            <div class="col-md-12 mt-4">
+                <h4 style="margin-left: 40px;"><b>For Company:</b></h4>
+                <ul class="mt-4">
+                    <li><p><img width="20" src="{{ asset('html/green_tick.png') }}"> Register as company</p></li>
+                    <li><p><img width="20" src="{{ asset('html/green_tick.png') }}"> Add staff members</p></li>
+                    <li><p><img width="20" src="{{ asset('html/green_tick.png') }}"> Create assignments and search for the right investigator from the available database.</p></li>
+                    <li><p><img width="20" src="{{ asset('html/green_tick.png') }}"> Filter investigators based on their availability, distance, rates etc.</p></li>
+                    <li><p><img width="20" src="{{ asset('html/green_tick.png') }}"> Chat with the shortlisted candidates.</p></li>
+                </ul>
+            </div>
+
+
+            <div class="col-md-12 mt-4">
+                <h4 style="margin-left: 40px;"><b>For Investigators:</b></h4>
+                <ul class="mt-4">
+                    <li><p><img width="20" src="{{ asset('html/green_tick.png') }}"> Register as an investigator</p></li>
+                    <li><p><img width="20" src="{{ asset('html/green_tick.png') }}"> Complete your profile</p></li>
+                    <li><p><img width="20" src="{{ asset('html/green_tick.png') }}"> Connect your google calendar for your availability.</p></li>
+                    <li><p><img width="20" src="{{ asset('html/green_tick.png') }}"> Get your profile viewed by companies looking for hiring.</p></li>
+                    <li><p><img width="20" src="{{ asset('html/green_tick.png') }}"> Get invites and chat before accepting any offer.</p></li>
+                </ul>
             </div>
         </div>
     </div>
