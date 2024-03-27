2c01f418042a5045ac5338d2160677ef953272ec
dev05 <devod0485@gmail.com>
2024-03-27 17:39:37 +0530

    updating db migration file

--- database/migrations/2024_03_26_100256_add_column_to_calendar_events.php
+++ database/migrations/2024_03_26_100256_add_column_to_calendar_events.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('calendar_events', function (Blueprint $table) {
-            $table->string('schedule')->after('title')->nullable();
+            $table->int('schedule')->after('title')->nullable();
         });
     }
 
